f467e374df0e9efba7599ba32df1a4c0278a570a
SJeanF <fonsecasantosjean@gmail.com>
2026-08-18 08:05:45 -0300

    refactor: remover algumas coisas desnecessarias e ajustar algumas imagens dos restaurantes

--- src/screens/RestaurantPage/RestaurantPage.jsx
+++ src/screens/RestaurantPage/RestaurantPage.jsx
@@ -4,16 +4,16 @@ import RestPageHero from '../../components/RestPageHero/RestPageHero'
 import DisplayRest from '../../components/DisplayRest/DisplayRest'
 import MenuItem from '../../components/MenuItem/MenuItem'
 import Footer from '../../components/Footer/Footer'
-import DishModal from '../../components/DishModal/DishModal'
 
-import { useState } from 'react'
 import { useParams } from 'react-router'
 
 import restInfos from '../../assets/restInfos.json'
 import restMenu from '../../assets/restMenus.json'
+import restDishImage from '../../assets/image-3.png'
+import restBack1Image from '../../assets/image-1-2.png'
+import restBack2Image from '../../assets/image-1.png'
 
 const RestaurantPerfil = () => {
-  const [modalState, setModalState] = useState({visible: false})
   
   const {id} = useParams()
 
@@ -24,23 +24,23 @@ const RestaurantPerfil = () => {
     <S.RestPageC>
       <S.TopC>
         <RestPageHero/>
-        {currRestInfos != null && <DisplayRest title={currRestInfos.titulo} type={currRestInfos.tipo} image={currRestInfos.capa}/>}
+        {currRestInfos != null && <DisplayRest title={currRestInfos.titulo} type={currRestInfos.tipo} image={id == 0? restBack1Image: restBack2Image}/>}
       </S.TopC>
       <S.MenuC>
         <S.MenuList>
-          {currMenuInfos != null && currMenuInfos.cardapio.map((curr) => <MenuItem key={`${currRestInfos.titulo}-dish-${curr.id}`}
+          {currMenuInfos != null && currMenuInfos.cardapio.map((curr) => <MenuItem 
+            key={`${currRestInfos.titulo}-dish-${curr.id}`}
             id={curr.id} 
             title={curr.nome} 
             desc={curr.descricao} 
-            image={curr.foto} 
+            image={restDishImage} 
             portion={curr.porcao} 
             price={curr.preco} 
-            setModal={setModalState}
+            setModal={null}
           />)}
         </S.MenuList>
       </S.MenuC>
       <Footer/>
-      {modalState.visible && <DishModal setModal={setModalState} dish={modalState.dish}/>}
     </S.RestPageC>
   )
 }
