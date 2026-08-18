f1c465800b3a75e2738686572de67c2ef61be416
SJeanF <fonsecasantosjean@gmail.com>
2026-08-18 08:13:23 -0300

    feat: adicionar navegação de volta para home

--- src/components/RestPageHero/RestPageHero.jsx
+++ src/components/RestPageHero/RestPageHero.jsx
@@ -3,11 +3,11 @@ import * as S from './styled'
 import logo from '../../assets/logo.png'
 
 
-const RestPageHero = () => {
+const RestPageHero = ({navigate}) => {
   return (
     <S.RestPageHeroC>
       <S.CenterC>
-        <S.HeroMessage>
+        <S.HeroMessage onClick={() => navigate()}>
           Restaurantes
         </S.HeroMessage>
         <S.Logo src={logo} />
